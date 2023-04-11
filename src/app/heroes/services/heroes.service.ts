165036076900b966a94eed02be057889812d7c61
PabloSadura <pablosadura@gmail.com>
2023-04-11 11:48:04 -0300

    Se crearon environments

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Heroe } from '../interfaces/heroes.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +11,13 @@ import { Heroe } from '../interfaces/heroes.interface';
 export class HeroesService {
   constructor(private http: HttpClient) {}
 
-  URL: string = 'http://localhost:3000/heroes';
+  private baseURL: string = environment.baseUrl;
 
   getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>(this.URL);
+    return this.http.get<Heroe[]>(`${this.baseURL}/heroes`);
   }
 
   getHeroePorId(id: string): Observable<Heroe> {
-    return this.http.get<Heroe>(`${this.URL}/${id}`);
+    return this.http.get<Heroe>(`${this.baseURL}/heroes/${id}`);
   }
 }
